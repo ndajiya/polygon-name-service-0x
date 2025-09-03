356582686ec8b4c8412258704f523a11c5471368
ndajiyas <ndajiyas@gmail.com>
2025-09-03 12:02:20 +0000

    Added kairos

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 import contractAbi from './contractABI.json';
 
-export const tld = '.w3';
+export const tld = '.kairos';
 export const CONTRACT_ADDRESS = '0x7A76442d7489317c6aC39DfBe6A4219E04555C50';
 export const abi = contractAbi.abi;
